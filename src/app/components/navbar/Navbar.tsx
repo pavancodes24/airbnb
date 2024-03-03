d1c50c8500bfd2fef4ee93a394e906105bbb2745
chandler <pavanchandupatla24@gmail.com>
2024-03-03 11:20:59 +0530

    authenticaltion done

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -2,8 +2,14 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import { SafeUser } from "@/app/types";
 
-const Navbar = () => {
+
+interface NavbarProps {
+    currentUser?: SafeUser | null;
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
     return (
         <div className='fixed w-full bg-white z-10 shadow-sm'>
             <div
@@ -19,7 +25,7 @@ const Navbar = () => {
                     >
                         <Logo />
                         <Search />
-                        <UserMenu />
+                        <UserMenu currentUser={currentUser}/>
 
                     </div>
                 </Container>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,23 @@ import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
-const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Airbnb",
   description: "Airbnb clone",
 };
 
-export default function RootLayout({
+const font = Nunito({ subsets: ["latin"] });
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentUser  = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -26,7 +30,7 @@ export default function RootLayout({
         <ToasterProvider />
         <LoginModal />
         <RegisterModal/>
-        <Navbar />
+        <Navbar  currentUser = {currentUser}/>
         <div className="pb-20 pt-28">
         {children}
         </div>
